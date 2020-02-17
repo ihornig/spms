0c033c188d70ca93c58ed83d2bc1b76c4e82beec
jbindrim <jbindrim>
2020-02-17 13:00:42 +0100

    ...

---
+++ spms-v.1.2/src/main/java/datenbank/Account.java
@@ -0,0 +1,40 @@
+package datenbank;
+
+import java.io.*;
+import javax.persistence.*;
+
+@NamedQuery(name = "SelectAccount", query = "Select k from Account k")
+@Entity
+public class Account implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String username;
+	private String password;
+	
+	public Account() {}
+	
+	public Account(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+}
