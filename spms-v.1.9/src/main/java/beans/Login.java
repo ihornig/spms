47b02b0d2e72c4097e81e493febb1abd7b28f732
ihornig <57348809+ihornig@users.noreply.github.com>
2020-02-24 00:58:04 +0100

    login/logout + Rolle mit Zugriffsrechten

---
+++ spms-v.1.9/src/main/java/beans/Login.java
@@ -0,0 +1,104 @@
+package beans;
+
+import datenbank.Account;
+import beans.AccountHandler;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.faces.model.*;
+import javax.persistence.*;
+import javax.transaction.UserTransaction;
+import javax.servlet.http.*;
+
+@ManagedBean(name = "loginHandler")
+@SessionScoped
+public class Login implements Serializable, Cloneable {
+	
+	private String usernamelogin;
+	private String passwordlogin;
+
+	String temp = "test";
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Resource
+	private UserTransaction utx;
+
+	public void setUsernamelogin(String usernamelogin) {
+		this.usernamelogin = usernamelogin;
+	}
+
+	public String getUsernamelogin() {
+		return usernamelogin;
+	}
+
+	public void setPasswordlogin(String passwordlogin) {
+		this.passwordlogin = passwordlogin;
+	}
+
+	public String getPasswordlogin() {
+		return passwordlogin;
+	}
+
+	private String username;
+	private String password;
+	private String rolle;
+	private Account kunde;
+	
+	public String login() {
+		Query query = em.createQuery("select k from Account k where k.username = :username and k.password = :password");
+		query.setParameter("username", usernamelogin);
+		query.setParameter("password", passwordlogin);
+		List<Account> kunden = query.getResultList();
+		if (kunden.size() == 1) {
+			kunde = kunden.get(0);
+			username = kunde.getUsername();
+			return "/startseite.xhtml?faces-redirect=true";
+		} else {
+			return null;
+		}
+	}
+	
+	public void checkLoggedIn(ComponentSystemEvent cse) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		if (kunde == null) {
+			context.getApplication().getNavigationHandler().handleNavigation(context, null,"/login.xhtml?faces-redirect=true");
+		}
+	}
+	
+	public void checkLoggedInLogin(ComponentSystemEvent cse) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		if (kunde != null) {
+			context.getApplication().getNavigationHandler().handleNavigation(context, null,"/startseite.xhtml?faces-redirect=true");
+		}
+	}
+	
+	public void checkRangVerwalter(ComponentSystemEvent cse) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		Query query = em.createQuery("select k from Account k where k.username = :username");
+		query.setParameter("username", username);
+		List<Account> kunden = query.getResultList();
+		if (kunden.size() == 1) {
+			kunde = kunden.get(0);
+		}
+		if (kunde == null) {
+			context.getApplication().getNavigationHandler().handleNavigation(context, null,"/login.xhtml?faces-redirect=true");
+		} else if (kunde.getRolle() == "user") {
+			context.getApplication().getNavigationHandler().handleNavigation(context, null,"/startseite.xhtml?faces-redirect=true");
+		}
+	}
+	
+	public String logout () {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/login.xhtml?faces -redirect=true";
+	}
+	
+}
---
+++ spms-v.1.9/src/main/java/datenbank/Termin.java
@@ -0,0 +1,58 @@
+package datenbank;
+
+import java.io.*;
+import javax.persistence.*;
+
+@NamedQuery(name = "SelectTermin", query = "Select t from Termin t")
+@Entity
+
+public class Termin implements Serializable{
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private Integer id;	
+		
+		private String titel;
+		private String datum;
+		private String beginn;
+		private String ende;
+		private String beschreibung;
+		
+		public Termin() {}
+		public Termin(String titel,String datum,String beginn,String ende,String beschreibung){
+			this.titel = titel;
+			this.datum = datum;
+			this.beginn = beginn;
+			this.ende = ende;
+			this.beschreibung = beschreibung;
+		}
+		public void setTitel(String titel) {
+			this.titel = titel;
+		}
+		public void setDatum(String datum) {
+			this.datum = datum;
+		}
+		public void setBeginn(String beginn) {
+			this.beginn = beginn;
+		}
+		public void setEnde(String ende) {
+			this.ende = ende;
+		}
+		public void setBeschreibung(String beschreibung) {
+			this.beschreibung = beschreibung;
+		}
+		public String getTitel() {
+			return titel;
+		}
+		public String getDatum() {
+			return datum;
+		}
+		public String getBeginn() {
+			return beginn;
+		}
+		public String getEnde() {
+			return ende;
+		}
+		public String getBeschreibung() {
+			return beschreibung;
+		}
+	}
