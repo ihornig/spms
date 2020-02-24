8b7b1b26c63a5025c3fb696538bf4970dc069790
leonvoss976 <60973889+leonvoss976@users.noreply.github.com>
2020-02-24 22:00:04 +0100

    Style der Navbar in CSS-Datei ausgelagert + Termine löschen - Leon
    Version 1.12

---
+++ spms-v.1.12/src/main/java/beans/GruppeHandler.java
@@ -0,0 +1,133 @@
+package beans;
+
+import datenbank.Gruppe;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.event.Event;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.*;
+import javax.inject.Inject;
+import javax.persistence.*;
+import javax.transaction.UserTransaction;
+import java.util.*;
+import events.GroupDeletionEvent;
+
+@ManagedBean(name = "gruppeHandler")
+@SessionScoped
+public class GruppeHandler {
+
+	public GruppeHandler(){
+		tempGruppe = new Gruppe();
+	}
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Resource
+	private UserTransaction utx;
+	
+	private DataModel<Gruppe> gruppen;
+	private Gruppe tempGruppe = new Gruppe();
+	private String name;
+	
+	private List<String> nameholder = new ArrayList<String>();
+	
+	@Inject
+	Event<GroupDeletionEvent> deleteEvent;
+	
+	public void removeGroup() {
+		boolean found = false;
+		int i = 0;
+		while(!found && i < gruppen.getRowCount()) {
+			gruppen.setRowIndex(i++);
+			tempGruppe = gruppen.getRowData();
+			if(tempGruppe.getName().equals(name)) {
+				found = true;
+			}
+		}
+		try {
+			utx.begin();
+		} catch (Exception exc) {}
+		tempGruppe = em.merge(tempGruppe);
+		em.remove(tempGruppe);
+		gruppen.setWrappedData(em.createNamedQuery("SelectGruppe").getResultList());
+		try {
+			utx.commit();
+		} catch (Exception exc) {}
+		updateNameholder();
+		deleteEvent.fire(new GroupDeletionEvent(name));
+	}
+	
+	@PostConstruct
+	public void init() {
+		try {
+			utx.begin();
+		} catch (Exception exc) {}
+		gruppen = new ListDataModel<Gruppe>();
+		gruppen.setWrappedData(em.createNamedQuery("SelectGruppe").getResultList());
+		try {
+			utx.commit();
+		} catch (Exception exc) {}
+		updateNameholder();
+	}
+	
+	public void speichern() {
+		for(String name : nameholder) {
+			if(tempGruppe.getName().equals(name)) {
+				return;
+			}
+		}
+		try {
+			utx.begin();
+		} catch (Exception exc) {}
+		tempGruppe = em.merge(tempGruppe);
+		em.persist(tempGruppe);
+		gruppen.setWrappedData(em.createNamedQuery("SelectGruppe").getResultList());
+		try {
+			utx.commit();
+		} catch (Exception exc) {}
+		tempGruppe = new Gruppe();
+		updateNameholder();
+	}
+	
+	private void updateNameholder() {
+		nameholder.clear();
+		for(Gruppe gruppe : gruppen) {
+			nameholder.add(gruppe.getName());
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNameholder(List<String> name){
+		nameholder = name;
+	}
+	
+	public List<String> getNameholder() {
+		return nameholder;
+	}
+	
+	public DataModel<Gruppe> getGruppen(){
+		return gruppen;
+	}
+	
+	public void setGruppen(DataModel<Gruppe> gruppen) {
+		this.gruppen = gruppen;
+	}
+	
+	public void setTempGruppe(Gruppe gruppe) {
+		this.tempGruppe = gruppe;
+	}
+	
+	public Gruppe getTempGruppe() {
+		return tempGruppe;
+	}
+	
+}
